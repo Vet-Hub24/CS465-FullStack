e2f424445f2f8e949e8ed2f6db53dbc1c39d0e45
Vet-Hub24 <Jake.Gagnon@snhu.edu>
2026-07-15 16:30:30 -0400

    Module 3 completed baseline

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var hbs = require('hbs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server','views', 'partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
